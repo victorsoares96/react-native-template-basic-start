ddc9adc062f3f690aece818e346d46b394783367
João Victor Moreira Soares <vitorsoares96@hotmail.com>
2021-10-24 15:33:27 -0300

    publish

--- template.config.js
+++ template.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "project_name",
+  placeholderName: "ProjectName",
 
   // Directory with template
   templateDir: "./template",
-
-  // Path to script, which will be executed after init
-  // postInitScript: "./script.js"
 };
--- template/ios/ProjectNameTests/testingrnappsTests.m
+++ template/ios/ProjectNameTests/testingrnappsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testingrnappsTests : XCTestCase
+@interface projectnameTests : XCTestCase
 
 @end
 
-@implementation testingrnappsTests
+@implementation projectnameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
