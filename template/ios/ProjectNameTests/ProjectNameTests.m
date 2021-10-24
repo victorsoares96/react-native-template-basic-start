19e09215c1e0f36ef1f2476c4854db0d52c31eaf
João Victor Moreira Soares <vitorsoares96@hotmail.com>
2021-10-24 16:39:09 -0300

    publish

--- template/ios/ProjectNameTests/ProjectNameTests.m
+++ template/ios/ProjectNameTests/ProjectNameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface projectnameTests : XCTestCase
+@interface ProjectNameTests : XCTestCase
 
 @end
 
-@implementation projectnameTests
+@implementation ProjectNameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
