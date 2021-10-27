41323575cb5712011656a02fd0504c35590fad2e
João Victor Moreira Soares <vitorsoares96@hotmail.com>
2021-10-27 14:37:45 -0300

    modified:   template/android/app/build.gradle 	new file:   template/android/app/src/main/assets/index.android.bundle 	modified:   template/android/gradle.properties 	modified:   template/package.json 	new file:   template/scripts/debug_android.sh 	new file:   template/scripts/release_android.sh

---
+++ template/scripts/release_android.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -rf ./android/app/src/main/res/drawable-*
+rm -rf ./android/app/src/main/res/raw
+
+npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle 
+--assets-dest android/app/src/main/res/
+
+cd android
+./gradlew clean assembleRelease -Dorg.gradle.daemon=false
